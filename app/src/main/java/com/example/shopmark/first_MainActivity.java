eca24a8515854d1211a57dba9b93ab6e946f92e9
Raul Fabricio Martinez Ramos <u19218420@utp.edu.pe>
2022-11-19 13:10:12 -0500

    First Load

---
+++ app/src/main/java/com/example/shopmark/first_MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shopmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class first_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+    }
+}
